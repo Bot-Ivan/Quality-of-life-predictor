5e3c6bf057fef9ad4c125f7d231644b472155721
Ivan Figueroa <Ivanfiggomez@gmail.com>
2023-09-06 18:39:59 -0400

    created prepare_country_states
    The purpose of this function is to merge the appropiate data from the 2 data frames into one
    
    -currently LSF's data has been trimmed to only take into consideration Life satisfaction. It

--- Quality of life predictor.py
+++ Quality of life predictor.py
@@ -3,7 +3,16 @@ import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd
 import sklearn.linear_model
+import os
 
+def prepare_country_stats(LSF, GPCF): #Life Satisfaction File, GDP Per Capita File
+    LSF = LSF[LSF["INEQUALITY"]=="TOT"]
+    LSF = LSF[LSF["Indicator"] == "Life satisfaction"]
+    LSF = LSF.pivot(index = "Country", columns = "Indicator", values = "Value")
+    print(LSF)
 
-BLI = pd.read("BLI_02092023235650521.csv")
-gdp_per_capita = pd.read_csv("gdp_per_capita.csv", delimiter = "\t", encoding='latin1', na_values = "n/a")
+    
+
+BLI = pd.read_csv("BLI.csv")
+gdp_per_capita = pd.read_csv("gdp_per_capita.csv", delimiter = "\t")
+prepare_country_stats(BLI, gdp_per_capita)
